aef8ab0a75e6f03f05925220533d1d9e157cd87b
Arch1e156 <yegorik2003@gmail.com>
2023-05-02 18:07:56 +0300

    SceneItem and ItemDescriptor

---
+++ Assets/Scripts/Items/Data/ItemDescriptor.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+[Serializable]
+public class ItemDescriptor
+{
+    //[field: SerializeField] public ItemId ItemId { get; private set; }
+    //[field: SerializeField] public ItemType ItemType { get; private set; }
+    [field: SerializeField] public Sprite ItemSprite { get; private set; }
+    //public ItemDescriptor(ItemId itemId, ItemType itemType, Sprite itemSprite)
+//   {
+//       ItemId = itemId;
+//       ItemType = itemType;
+//       ItemSprite = itemSprite;
+//   }
+}
