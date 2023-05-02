36b2d952e9055dbb647825621033b86cbd1f5b88
d1nque <denisgark@icloud.com>
2023-05-02 19:38:34 +0300

    data and item prefab

--- Assets/Scripts/Items/Data/ItemDescriptor.cs
+++ Assets/Scripts/Items/Data/ItemDescriptor.cs
@@ -5,13 +5,13 @@ using static UnityEditor.Progress;
 [Serializable]
 public class ItemDescriptor
 {
-    //[field: SerializeField] public ItemId ItemId { get; private set; }
-    //[field: SerializeField] public ItemType ItemType { get; private set; }
+    [field: SerializeField] public ItemId ItemId { get; private set; }
+    [field: SerializeField] public ItemType ItemType { get; private set; }
     [field: SerializeField] public Sprite ItemSprite { get; private set; }
-    //public ItemDescriptor(ItemId itemId, ItemType itemType, Sprite itemSprite)
-//   {
-//       ItemId = itemId;
-//       ItemType = itemType;
-//       ItemSprite = itemSprite;
-//   }
+    public ItemDescriptor(ItemId itemId, ItemType itemType, Sprite itemSprite)
+  {
+      ItemId = itemId;
+      ItemType = itemType;
+      ItemSprite = itemSprite;
+  }
 }
---
+++ Assets/Scripts/Items/Data/ItemId.cs
@@ -0,0 +1,9 @@
+
+public enum ItemId 
+{
+    None = 0,
+    Heart = 1,
+    Armor = 2,
+    Sword = 3,
+    Coin = 4,
+}
