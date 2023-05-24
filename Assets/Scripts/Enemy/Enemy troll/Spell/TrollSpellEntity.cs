e0043a60b04dbbb2072081d8ab08fd370337780a
Arch1e156 <yegorik2003@gmail.com>
2023-05-24 21:53:03 +0300

    Troll spell prephab + entity

---
+++ Assets/Scripts/Enemy/Enemy troll/Spell/TrollSpellEntity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollSpellEntity : MonoBehaviour
+{
+    [SerializeField] public Transform _rangeAttackPoint;
+    [SerializeField] public float _rangeAttackDamage;
+    [SerializeField] public float _rangeAttackRadius;
+    [SerializeField] private GameObject _player;
+    private Vector2 _spellSpawner;
+    private void Awake()
+    {
+        _player = GameObject.Find("Player");
+    }
+    private void Start()
+    {
+        _spellSpawner = new Vector2(_player.transform.position.x, transform.position.y);
+        transform.position = _spellSpawner;
+    }
+    private void SpellDamager()
+    {
+        var targetCollider = Physics2D.OverlapCircle(_rangeAttackPoint.position, _rangeAttackRadius);
+        //if (targetCollider != null && targetCollider.TryGetComponent(out IDamageble damageble)) 
+        //{
+        //    damageble.TakeDamage(_rangeAttackDamage);
+        //}
+    }
+    private void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
